02cc665a8e68beff5bb96872e7aa414f020ff28e
Matt Macdonald <mksmacdonald@gmail.com>
2022-11-28 16:39:30 -0800

    Added Axios and added post to onClick Prop

--- src/pages/SelectPattern.js
+++ src/pages/SelectPattern.js
@@ -1,11 +1,29 @@
 import PatternButton from "./components/PatternButton"
 
+import axios, { isCancel, AxiosError } from 'axios';
+
+function postPatternRequest() {
+    console.log("Poop")
+
+    axios.post('/user', {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
 function SelectPattern() {
     return (
-        <div className="SelectPattern">
-            This is the select pattern page.
-            <PatternButton buttonText="Rainbow Cycle" />
+        < div className="SelectPattern" >
+            Select the pattern you'd like to use.
+            <div>
+                <PatternButton buttonText="Rainbow Cycle" onClick={postPatternRequest} />
+            </div>
         </div >
     );
 }
--- src/pages/components/PatternButton.js
+++ src/pages/components/PatternButton.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
 export default function PatternButton(props) {
     const classes = useStyles();
     return (
-        <ColorButton variant="contained" color="primary" className={classes.margin}>
+        <ColorButton variant="contained" color="primary" className={classes.margin} onClick={props.onClick}>
             {props.buttonText}
         </ColorButton>
 
