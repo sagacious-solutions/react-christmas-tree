13340f6a83196b99d317a7a12907986c7e5164af
Matt Macdonald <mksmacdonald@gmail.com>
2022-11-30 10:33:32 -0800

    Completed choose solid preset page

---
+++ src/stories/SelectPattern.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SelectPattern from "./SelectPattern";
+
+export default {
+  title: "ChristmasTouchScreen/Page/SelectPattern",
+  component: SelectPattern,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+};
+
+const Template = (args) => <SelectPattern {...args} />;
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+export const Default = Template.bind({});
