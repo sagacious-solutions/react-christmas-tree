f9bfb5f000c3c3ab0255367dbcb83fc60bbd0f38
Matt Macdonald <mksmacdonald@gmail.com>
2022-12-10 11:20:36 -0800

    improved formatting

--- src/pages/CustomPattern.js
+++ src/pages/CustomPattern.js
@@ -1,37 +1,110 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-import ColorPicker from "./components/ColorPicker";
 import BasicButton from "./components/BasicButton";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import {
+    postColorRequest,
+    postCustomPatternRequest,
+} from "../serverCommunication";
+import { SketchPicker } from "react-color";
 
-const CHRISTMAS_TREE_URL = process.env.REACT_APP_CHRISTMAS_TREE_URL;
+const buttonStyle = {};
+
+function getRgb(color) {
+    const r = color.rgb.r;
+    const g = color.rgb.g;
+    const b = color.rgb.b;
+    return [r, g, b];
+}
+
+function generatePatternDots(pattern) {
+    let avatars = [];
+
+    pattern.forEach((color, index) => {
+        avatars.push(
+            <div style={{ marginTop: "0.5em" }}>
+                <Avatar
+                    key={`color-circle-${index}`}
+                    sx={{
+                        backgroundColor: color.hex,
+                        marginLeft: index ? "0px" : "10px",
+                    }}
+                    children={""}
+                />
+            </div>
+        );
+    });
+
+    return avatars;
+}
+
+function sendPatternToServer(pattern) {
+    const rgbList = [];
+
+    pattern.forEach((color) => {
+        rgbList.push([color.rgb.r, color.rgb.g, color.rgb.b]);
+    });
+
+    postCustomPatternRequest(rgbList);
+}
 
 function CustomPattern() {
-    const [color, setColor] = useState({ color: { r: 50, g: 0, b: 0 } });
+    const [pattern, setPattern] = useState([]);
+    const [color, setColor] = useState({ rgb: { r: 50, g: 0, b: 0 } });
+    const [rgb, setRgb] = useState(0);
 
     const handleColorChange = (color, _event) => {
         setColor(color);
     };
 
-    useEffect(() => {}, []);
-
-    function postColorRequest(color) {
-        axios
-            .post(CHRISTMAS_TREE_URL + "/setRgbColor/", {
-                color: color,
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+    useEffect(() => {
+        setRgb(getRgb(color));
+    }, [color]);
 
     return (
         <div>
             Make your own pattern.
-            <ColorPicker onChange={handleColorChange} color={color} />
-            <BasicButton
-                style={{ backgroundColor: color.hex }}
-                buttonText={"Preview Color"}
-            />
+            <SketchPicker onChange={handleColorChange} color={color} />
+            <Stack
+                style={{
+                    display: "flex",
+                    marginTop: 50,
+                    height: "4em",
+                    backgroundColor: "grey",
+                }}
+                direction="row"
+                spacing={2}
+            >
+                {generatePatternDots(pattern)}
+            </Stack>
+            <div
+                style={{
+                    marginTop: 50,
+                    display: "Flex",
+                    justifyContent: "space-evenly",
+                }}
+            >
+                <BasicButton
+                    style={{}}
+                    buttonText={"Preview Color"}
+                    onClick={() => postColorRequest(rgb)}
+                />
+                <BasicButton
+                    style={{}}
+                    buttonText={"Add color to Pattern"}
+                    onClick={() => setPattern([...pattern, color])}
+                />
+                <BasicButton
+                    style={{}}
+                    buttonText={"Set tree to Pattern"}
+                    onClick={() => sendPatternToServer(pattern)}
+                />
+                <BasicButton
+                    style={{}}
+                    buttonText={"Clear Pattern"}
+                    onClick={() => setPattern([])}
+                />
+            </div>
         </div>
     );
 }
